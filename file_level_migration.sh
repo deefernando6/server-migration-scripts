f31d7ec8b4fa93d97941c8e1c167e5f225a8e3d4
deeghayu <deeghayu@orangehrm.com>
2025-01-22 19:49:38 +0530

    Added the prompts for the user inputs

--- file_level_migration.sh
+++ file_level_migration.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 # Make sure to execute this script as root
+
+# Prompt for the server details
+read -p "Enter the local IP address of the migrating server: " SERVER_IP
+read -sp "Enter the root password of the migrated server: " SERVER_PASS
