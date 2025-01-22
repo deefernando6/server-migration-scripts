18dcdd507d0c7030280f575c1acfa4afb9c46201
deeghayu <deeghayu@orangehrm.com>
2025-01-22 14:14:15 +0530

    Added the exporting mysql users and taking dump steps

--- database_migration.sh
+++ database_migration.sh
@@ -1,14 +1,36 @@
-# Make sure to execute this script as root
-
-
 #!/bin/bash
+
+# Make sure to execute this script as root
 
 # Prompt for the location to save dumps on the current server
 read -p "Enter the directory to save database dumps on current server: " DUMP_DIR
 
-#prompt for the server details
+# Prompt for the server details
 read -p "Enter the IP address of the migrating server: " SERVER_IP
 read -sp "Enter the root password of the migrated server: " SERVER_PASS
 
-#prompt for the location to save dumps on migrated server
+# Prompt for the location to save dumps on migrated server
 read -p "Enter the directory to save database dumps on migrated server: " SOURCE_DIR
+
+
+# Take dumps of all MariaDB databases on the current server
+echo "Fetching database list from the current server"
+DATABASES=$(mysql -e 'SHOW DATABASES;' | grep -v Database | grep -v information_schema | grep -v performance_schema)
+die_on_fail "Failed to fetch database list from the current server"
+
+# Taking the mysql dumps 
+echo "Starting database dumps on the current server"
+for DB in $DATABASES; do
+    echo "Taking dump of database: $DB..."
+    mysqldump -h localhost --triggers --routines --events --hex-blob $DB | sed 's/\`$DB\`\.//g' > $DUMP_DIR/${DB}_dump.sql
+    die_on_fail "Failed to take dump of database: $DB"
+    echo "Dump of database: $DB completed successfully"
+done
+
+#Exporting MySQL users
+
+echo "Exporting MySQL users from the current server..."
+"mysql -e 'SELECT CONCAT(\"CREATE USER '\", user, \"'@'\", host, \"' IDENTIFIED BY PASSWORD '\", authentication_string, \"';\") FROM mysql.user;' > $DUMP_DIR/mysql_users.sql"
+die_on_fail "Failed to export MySQL users"
+echo "MySQL users exported successfully"
+
