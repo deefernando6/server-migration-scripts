6e62232071d70bb1ab5661df126fc4c96ecf0812
deeghayu <deeghayu@orangehrm.com>
2025-01-22 21:09:18 +0530

    Added the syncing the nginx vhosts

--- file_level_migration.sh
+++ file_level_migration.sh
@@ -5,3 +5,21 @@
 # Prompt for the server details
 read -p "Enter the local IP address of the migrating server: " SERVER_IP
 read -sp "Enter the root password of the migrated server: " SERVER_PASS
+
+
+echo "Starting rsync of codebases from the current server to the migration server"
+
+# Getting all the codebases list from the current server to migration server
+DIRECTORIES=$(ls -d /var/www/html/OHRMStandalone/PROD/*/)
+
+# Syncing the directories from current server to migration
+for DIR in $DIRECTORIES; do
+    BASENAME=$(basename "$DIR")
+    echo "Syncing directory: $BASENAME..."
+    sshpass -p $SERVER_PASS rsync -av -o --append --progress -e "ssh -p 2112" $DIR/ root@$SERVER_IP:/var/www/html/OHRMStandalone/PROD/
+    die_on_fail "Failed to sync directory: $BASENAME"
+    echo "Directory: $BASENAME synced successfully"
+done
+
+#Syncing the nginx vhosts from the current server to the migration server
+sshpass -p $SERVER_PASS rsync -av -o --append --progress -e "ssh -p 2112" /etc/nginx/vhosts/ root@$SERVER_IP:/etc/nginx/
