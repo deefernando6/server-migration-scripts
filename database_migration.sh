cb36d8832638025c7777737bac7cb6435aa0f5ce
deeghayu <deeghayu@orangehrm.com>
2025-01-22 12:42:28 +0530

    Added the prompts to get the user inputs

--- database_migration.sh
+++ database_migration.sh
@@ -3,3 +3,12 @@
 
 #!/bin/bash
 
+# Prompt for the location to save dumps on the current server
+read -p "Enter the directory to save database dumps on current server: " DUMP_DIR
+
+#prompt for the server details
+read -p "Enter the IP address of the migrating server: " SERVER_IP
+read -sp "Enter the root password of the migrated server: " SERVER_PASS
+
+#prompt for the location to save dumps on migrated server
+read -p "Enter the directory to save database dumps on migrated server: " SOURCE_DIR
