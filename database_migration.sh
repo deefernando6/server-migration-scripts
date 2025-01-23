43987e381b7b080c9a2504ee7d41315740293954
deeghayu <deeghayu@orangehrm.com>
2025-01-23 11:12:25 +0530

    Updated the mysql dump command

--- database_migration.sh
+++ database_migration.sh
@@ -4,10 +4,10 @@
 
 # Prompt for the location to save dumps on the current server
 read -p "Enter the directory to save database dumps on current server: " DUMP_DIR
-
+read -sp "Enter the mysql mysql root password of the migrated server: " DBPASS
 # Prompt for the server details
 read -p "Enter the local IP address of the migrating server: " SERVER_IP
-read -sp "Enter the root password of the migrated server: " SERVER_PASS
+read -sp "Enter the mysql root user password of the migrated server: " SERVER_PASS
 
 # Prompt for the location to save dumps on migrated server
 read -p "Enter the directory to save database dumps on migrated server: " MIGRATE_DIR
@@ -22,20 +22,20 @@ die_on_fail "Failed to fetch database list from the current server"
 echo "Starting database dumps on the current server"
 for DB in $DATABASES; do
     echo "Taking dump of database: $DB..."
-    mysqldump -h localhost --triggers --routines --events --hex-blob $DB | sed 's/\`$DB\`\.//g' > $DUMP_DIR/${DB}_dump.sql
+    mysqldump -h localhost -u root -p$DBPASS --triggers --routines --events --hex-blob $DB | sed 's/\`$DB\`\.//g' > $DUMP_DIR/${DB}_dump.sql
     die_on_fail "Failed to take dump of database: $DB"
     echo "Dump of database: $DB completed successfully"
 done
 
 #Exporting MySQL users
 echo "Exporting MySQL users from the current server..."
-"mysql -e 'SELECT CONCAT(\"CREATE USER '\", user, \"'@'\", host, \"' IDENTIFIED BY PASSWORD '\", authentication_string, \"';\") FROM mysql.user;' > $DUMP_DIR/mysql_users.sql"
+"mysql -e -h localhost -u root -p$DBPASS 'SELECT CONCAT(\"CREATE USER '\", user, \"'@'\", host, \"' IDENTIFIED BY PASSWORD '\", authentication_string, \"';\") FROM mysql.user;' > $DUMP_DIR/mysql_users.sql"
 die_on_fail "Failed to export MySQL users"
 echo "MySQL users exported successfully"
 
 #Tranferring all the mysql dumps and the mysql users to the migration server.
 echo "Transferring MySQL users dump..."
-sshpass -p $SERVER_PASSrsync -av -o --append --progress -e "ssh -p 2112" $DUMP_DIR/ root@$SERVER_IP:$DUMP_DIR/
+sshpass -p $SERVER_PASS rsync -av -o --append --progress -e "ssh -p 2112" $DUMP_DIR/ root@$SERVER_IP:$DUMP_DIR/
 die_on_fail "Failed to transfer MySQL users dump"
 echo "MySQL users dump transferred successfully"
 
