5d12e3c07778a7707cb20cfff1ea77838cf37880
deeghayu <deeghayu@orangehrm.com>
2025-01-23 11:17:43 +0530

    Added the user input prompts

--- database_source.sh
+++ database_source.sh
@@ -2,3 +2,6 @@
 
 # Make sure to execute this script as root
 
+# Prompt for the location to save dumps on the current server
+read -p "Enter the directory where you keep the database dumps: " DUMP_DIR
+read -sp "Enter the mysql mysql root password of the migrated server: " DBPASS
